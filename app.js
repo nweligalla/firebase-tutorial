dbe4b38740e09ca9a9b87fe520dbf37a7bb39a10
nweligalla <nayanaweligalla@gmail.com>
2020-11-22 13:27:39 +0530

    firebase Tutorial from netninja

---
+++ app.js
@@ -0,0 +1,63 @@
+const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-addcafeform");
+
+function renderCafe(doc) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+  let cross = document.createElement("div");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+  cross.textContent = "x";
+
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross);
+
+  cafeList.appendChild(li);
+
+  //deleting-data
+  cross.addEventListener("click", (evt) => {
+    let id = evt.target.parentElement.getAttribute("data-id");
+    db.collection("cafes").doc(id).delete();
+  });
+}
+// getting data
+// db.collection("cafes")
+//   .where("city", "==", "Marioland")
+//   .orderBy("name")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       renderCafe(doc);
+//     });
+//   });
+
+//saving-data
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  db.collection("cafes").add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  form.name.value = "";
+  form.city.value = "";
+});
+
+//real-time listener
+db.collection("cafes")
+  .orderBy("city")
+  .onSnapshot((snapshot) => {
+    let changes = snapshot.docChanges();
+    changes.forEach((change) => {
+      if (change.type == "added") {
+        renderCafe(change.doc);
+      } else if (change.type == "removed") {
+        let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
+        cafeList.removeChild(li);
+      }
+    });
+  });
